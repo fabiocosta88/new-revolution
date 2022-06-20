343a24ac308ac41da1339d55965c6da153af086b
Fábio Costa <fabiocosta2907@hotmail.com>
2022-06-20 18:42:46 -0300

    items and imbuements rework

--- data/npc/addoner.lua
+++ data/npc/addoner.lua
@@ -1,7 +1,7 @@
 local addon_config = {
     [1] = {
         name = "Assassin Outfit",
-        achivement = "nome do achievmet, se tiver dois pfvr colocar achivment_1 = blabolabla, achivment_2 = blablabla, um emabixo do outro",
+        achivement = "Swift Death",
         outfit = {
             male = 152,
             female = 156
@@ -14,6 +14,456 @@ local addon_config = {
         second = {
             items = {
                 {5930,1},{5804,1}
+            }
+        }
+    },
+    [2] = {
+        name = "Barbarian Outfit",
+        achivement = "Brutal Politeness",
+        outfit = {
+            male = 143, 
+            female = 147
+        },
+        first = {
+            items = {
+                {5880,10},{5892,1},{5893,50},{5876,50}
+            }
+        },
+        second = {
+            items = {
+                {5884,1},{5911,50},{5910,50},{5886,10},{5885,1}
+            }
+        }
+    },
+    [3] = {
+        name = "Battle Mage Outfit",
+        achivement = "Exalted Battle Mage",
+        outfit = {
+            male = 1069,
+            female = 1070 
+        },
+        first = {
+            items = {
+                {28792,5}
+            }
+        },
+        second = {
+            items = {
+                {28793,20}
+            }
+        }
+    },
+    [4] = {
+        name = "Beggar Outfit",
+        achivement = "Life on the Streets",
+        outfit = {
+            male = 153,
+            female = 157 
+        },
+        first = {
+            items = {
+                {5883,100},{3043,2} -- 3043 são 20k. Deixar dessa forma? 
+            }
+        },
+        second = {
+            items = {
+                {6107,1} -- Verificar quest!
+            }
+        }
+    },
+    [5] = {
+        name = "Brotherhood of Bones Outfit",
+        achivement = "Skull and Bones",
+        outfit = {
+            male = 278,
+            female = 279 
+        },
+        first = {
+            items = {
+                {6499,500}
+            }
+        },
+        second = {
+            items = {
+                {6499,1000} -- Entregar Shield também! - ID 6432
+            }
+        }
+    },
+    [6] = {
+        name = "Citizen Outfit",
+        achivement = "Exemplary Citizen",
+        outfit = {
+            male = 128, 
+            female = 136 
+        },
+        first = {
+            items = {
+                {5878,100}
+            }
+        },
+        second = {
+            items = {
+                {5890,100},{5902,50},{3374,1}
+            }
+        }
+    },
+    [7] = {
+        name = "Dream Warden Outfit",
+        achivement = "Dream Warden",
+        outfit = {
+            male = 577, 
+            female = 578 
+        },
+        first = {
+            items = {
+                {20276,1}
+            }
+        },
+        second = {
+            items = {
+                {20275,1}
+            }
+        }
+    },
+    [8] = {
+        name = "Dream Warrior Outfit",
+        achivement = "Dream Warrior",
+        outfit = {
+            male = 1146,
+            female = 1147 
+        },
+        first = {
+            items = {
+                {30169,5}
+            }
+        },
+        second = {
+            items = {
+                {30168,1}
+            }
+        }
+    },
+    [9] = {
+        name = "Druid Outfit",
+        achivement = "Of Wolves and Bears",
+        outfit = {
+            male = 144,
+            female = 148
+        },
+        first = {
+            items = {
+                {5897,50},{5896,50}
+            }
+        },
+        second = {
+            items = {
+                {5937,1},{5938,1},{5906,100},{3012,1} -- Verificar 5938 deve estar cheio de água das Hydra lá... E aproveitar e verificar a possibilidade de vender o 5937 e 5938 (antes de serem utilizados) no NPC
+            }
+        }
+    },
+    [10] = {
+        name = "Elementalist Outfit",
+        achivement = "Mystic Fabric Magic",
+        outfit = {
+            male = 432, 
+            female = 433 
+        },
+        first = {
+            items = {
+                {12599,1}
+            }
+        },
+        second = {
+            items = {
+                {12803,1}
+            }
+        }
+    },
+    [11] = {
+        name = "Hunter Outfit",
+        achivement = "Hunting with Style",
+        outfit = {
+            male = 129, 
+            female = 137
+        },
+        first = {
+            items = {
+                {5947,1},{5876,100},{5882,100},{5891,5},{5887,1},{5889,1},{5888,1}
+            }
+        },
+        second = {
+            items = {
+                {5875,1}
+            }
+        }
+    },
+    [12] = {
+        name = "Jester Outfit",
+        achivement = "Fool at Heart",
+        outfit = {
+            male = 273, 
+            female = 270 
+        },
+        first = {
+            items = {
+                {5911,10},{5912,10},{5910,10},{5914,10}
+            }
+        },
+        second = {
+            items = {
+                {5911,15},{5912,15},{5910,15},{5914,15} -- Entregar também os itens 894 e 895 (Staff e HAT)
+            }
+        }
+    },
+    [13] = {
+        name = "Knight Outfit",
+        achivement = "In Shining Armor",
+        outfit = {
+            male = 131, 
+            female = 139
+        },
+        first = {
+            items = {
+                {5880,100},{5892,1}
+            }
+        },
+        second = {
+            items = {
+                {5893,100},{5924,1},{5885,1},{5887,1} -- Mudar ID da quest de 3351 para 5924 (banuta)
+            }
+        }
+    },
+    [14] = {
+        name = "Mage Outfit (Female) - Summoner Outfit (Male)",
+        achivement = "Ritualist",
+        outfit = {
+            male = 133, 
+            female = 138
+        },
+        first = {
+            items = {
+                {5958,1}
+            }
+        },
+        second = {
+            items = {
+                {5894,70},{5911,20},{5883,40},{5922,30},{5886,10},{5881,60},{5882,40},{5904,15},{5905,30}
+            }
+        }
+    },
+    [15] = {
+        name = "Mage Outfit (Male) - Summoner Outfit (Female)",
+        achivement = "Alumni",
+        outfit = {
+            male = 130,
+            female = 141
+        },
+        first = {
+            items = {
+                {3074,1},{3075,1},{3072,1},{3073,1},{3071,1},{3066,1},{3070,1},{3069,1},{3065,1},{3067,1},{5904,10},{3077,20},{5809,1}
+            }
+        },
+        second = {
+            items = {
+                {5903,1}
+            }
+        }
+    },
+    [16] = {
+        name = "Nightmare Knights Outfit",
+        achivement = "Nightmare Walker",
+        outfit = {
+            male = 268,
+            female = 269 
+        },
+        first = {
+            items = {
+                {6499,500}
+            }
+        },
+        second = {
+            items = {
+                {6499,1000} -- Entregar o Shield também! ID 6390
+            }
+        }
+    },
+    [17] = {
+        name = "Nobleman Outfit",
+        achivement = "Aristrocat",
+        outfit = {
+            male = 132, 
+            female = 140
+        },
+        first = {
+            items = {
+                {3043,15} -- 150k Verificar se assim funciona.
+            }
+        },
+        second = {
+            items = {
+                {3043,15} -- 150k Verificar se assim funciona.
+            }
+        }
+    },
+    [18] = {
+        name = "Norseman Outfit",
+        achivement = "Out in the Storm",
+        outfit = {
+            male = 251,
+            female = 252
+        },
+        first = {
+            items = {
+                {7290,5}
+            }
+        },
+        second = {
+            items = {
+                {7290,10}
+            }
+        }
+    },
+    [19] = {
+        name = "Oriental Outfit",
+        achivement = "One Thousand and One",
+        outfit = {
+            male = 146, 
+            female = 150 
+        },
+        first = {
+            items = {
+                {5945,1} -- Verificar se a quest está dando esse ID. 
+            }
+        },
+        second = {
+            items = {
+                {5883,100},{5895,100},{5891,2},{5912,100}
+            }
+        }
+    },
+    [20] = {
+        name = "Pirate Outfit",
+        achivement = "Swashbuckler",
+        outfit = {
+            male = 151,
+            female = 155 
+        },
+        first = {
+            items = {
+                {6098,100},{6126,100},{6097,100}
+            }
+        },
+        second = {
+            items = {
+                {6101,1},{6102,1},{6100,1},{6099,1}
+            }
+        }
+    },
+    [21] = {
+        name = "Rift Warrior Outfit",
+        achivement = "Rift Warrior",
+        outfit = {
+            male = 846, 
+            female = 845 
+        },
+        first = {
+            items = {
+                {22516,100}
+            }
+        },
+        second = {
+            items = {
+                {22516,100}
+            }
+        }
+    },
+    [22] = {
+        name = "Shaman Outfit",
+        achivement = "Way of the Shaman",
+        outfit = {
+            male = 154, 
+            female = 158
+        },
+        first = {
+            items = {
+                {3348,5},{3403,5}
+            }
+        },
+        second = {
+            items = {
+                {3002,5},{5014,1}
+            }
+        }
+    },
+    [23] = {
+        name = "Warmaster Outfit",
+        achivement = "Master of War",
+        outfit = {
+            male = 335,
+            female = 336
+        },
+        first = {
+            items = {
+                {10199,1}
+            }
+        },
+        second = {
+            items = {
+                {10198,1}
+            }
+        }
+    },
+    [24] = {
+        name = "Warrior Outfit",
+        achivement = "Wild Warrior",
+        outfit = {
+            male = 134, 
+            female = 142
+        },
+        first = {
+            items = {
+                {5925,100},{5899,100},{5884,1},{5919,1}
+            }
+        },
+        second = {
+            items = {
+                {5880,100},{5887,1}
+            }
+        }
+    },
+    [25] = {
+        name = "Wayfarer Outfit",
+        achivement = "Peazzekeeper",
+        outfit = {
+            male = 367, 
+            female = 366 
+        },
+        first = {
+            items = {
+                {11701,1}
+            }
+        },
+        second = {
+            items = {
+                {11700,1}
+            }
+        }
+    },
+    [26] = {
+        name = "Wizard Outfit",
+        achivement = "Warlock",
+        outfit = {
+            male = 145, 
+            female = 149 
+        },
+        first = {
+            items = {
+                {5922,50}
+            }
+        },
+        second = {
+            items = {
+                {3436,1},{3386,1},{3382,1},{3006,1}
             }
         }
     },
@@ -33,7 +483,6 @@ local function openAddonsChooseWindow(player)
     
     window:addButton(101, "Cancelar")
 	window:addButton(100, "Confirmar")
-
     for index, value in ipairs(addon_config) do
         window:addChoice(index, value.name)
     end
@@ -45,30 +494,66 @@ local function openAddonsChooseWindow(player)
     return true
 end
 
-local function openAddonNumberChoose(player) 
-    if not player then
-        return false
+local modalAddonsChoose = CreatureEvent("ModalWindow_AddonModal")
+modalAddonsChoose:type("modalwindow")
+
+function modalAddonsChoose.onModalWindow(player, modalWindowId, buttonId, choiceId)
+    player:unregisterEvent("ModalWindow_AddonModal")
+    if modalWindowId == 1008 then
+        if buttonId == 100 then
+            player:registerEvent("ModalWindow_AddonNumberchooseModal")
+ 
+            local title = addon_config[choiceId].name
+            local message = "Escolha o addon que deseja fazer.\nEste addon da o achivement: " .. addon_config[choiceId].achivement .. "."
+
+            local window = ModalWindow(1009, title, message)
+            
+            window:addButton(101, "Cancelar")
+            window:addButton(100, "Confirmar")
+
+            window:addChoice(1, "First addon")
+            window:addChoice(2, "Second addon")
+
+            window:setDefaultEnterButton(100)
+            window:setDefaultEscapeButton(101)
+        
+            window:sendToPlayer(player)
+        end
     end
-
-    player:registerEvent("ModalWindow_AddonNumberchooseModal")
- 
-    local title = "Assassin Outfit"
-    local message = "Escolha qual addon deseja fazer."
-
-    local window = ModalWindow(1008, title, message)
-    
-    window:addButton(101, "Cancelar")
-	window:addButton(100, "Confirmar")
-
-    window:addChoice(1, "First addon")
-    window:addChoice(2, "Second addon")
-
-    window:setDefaultEnterButton(100)
-    window:setDefaultEscapeButton(101)
- 
-    window:sendToPlayer(player)
     return true
 end
+
+modalAddonsChoose:register()
+
+
+local modalFinishAddon = CreatureEvent("ModalWindow_AddonNumberchooseModal")
+modalFinishAddon:type("modalwindow")
+
+function modalFinishAddon.onModalWindow(player, modalWindowId, buttonId, choiceId)
+    player:unregisterEvent("ModalWindow_AddonNumberchooseModal")
+    if modalWindowId == 1009 then
+        if buttonId == 100 then
+
+            player:registerEvent("ModalWindow_AddonChooseModal")
+ 
+            local title = "Requisitos do Addon"
+            local message = "Assasin outfit Addon 1\n50x Cloth xxx\n50x Cloth yyy"
+        
+            local window = ModalWindow(1010, title, message)
+            
+            window:addButton(101, "Voltar")
+            window:addButton(100, "Confirmar")
+        
+            window:setDefaultEnterButton(100)
+            window:setDefaultEscapeButton(101)
+         
+            window:sendToPlayer(player)
+        end
+    end
+    return true
+end
+
+modalFinishAddon:register()
 
 local function openSpecificAddon(player)
     if not player or addon then
@@ -80,7 +565,7 @@ local function openSpecificAddon(player)
     local title = "Requisitos do Addon"
     local message = "Assasin outfit Addon 1\n50x Cloth xxx\n50x Cloth yyy"
 
-    local window = ModalWindow(1009, title, message)
+    local window = ModalWindow(1010, title, message)
     
     window:addButton(101, "Voltar")
 	window:addButton(100, "Confirmar")
